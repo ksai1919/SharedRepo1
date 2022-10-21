41af0049542187633b5270a5b5429dddeda31532
ksai1919 <114721359+ksai1919@users.noreply.github.com>
2022-10-22 00:33:05 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,7 @@ def newdeploy(jobname,ip,context)
 {
   sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+def newdeploy(jobname)
+{
+   sh "/home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+}
